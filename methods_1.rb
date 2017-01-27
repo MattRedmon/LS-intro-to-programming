b09bc5f51825dbe3e9118c341e56c1e82bce7f50
Matt Redmon <mredmon333@gmail.com>
2017-01-26 20:16:50 -0500

    removing files moved into folders

--- methods_1.rb
+++
@@ -1,8 +0,0 @@
-# write a program that prints a greeting message
-
-def greeting(name)
-  puts "Buenos dias #{name}! Como esta usted?"
-  puts "Guten tag, #{name}. Wie gehts?"
-end
-greeting("Matt")
-greeting("Alice")
--- variables_name.rb
+++
@@ -1,5 +0,0 @@
-puts "Hello, tell my your name!"
-name = gets.chomp
-name.capitalize!
-
-puts "Hello #{name}, it's nice to meet you!"
